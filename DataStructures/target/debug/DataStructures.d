ef090e864cb24ef61cf949f29a169ab324e0752b
DESKTOP-KCS1KCG\Drako <mazzantia@hotmail.com>
2020-06-07 12:27:10 +0200

    Added - Slices and Touple

---
+++ DataStructures/target/debug/DataStructures.d
@@ -0,0 +1,1 @@
+D:\RustProjects\DataStructures\target\debug\DataStructures.exe: D:\RustProjects\DataStructures\src\main.rs
---
+++ DataStructures/target/debug/deps/DataStructures.d
@@ -0,0 +1,5 @@
+D:\RustProjects\DataStructures\target\debug\deps\DataStructures.exe: src\main.rs
+
+D:\RustProjects\DataStructures\target\debug\deps\DataStructures.d: src\main.rs
+
+src\main.rs:
